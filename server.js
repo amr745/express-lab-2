e6b1f2c20ee0d76ee96a38d03961b306e156429b
Ashley Richard <ashleymrichard@gmail.com>
2022-04-02 18:12:28 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express()
+const port = 3000
+
+app.get("/greeting/:name", (req, res) => {
+    res.send(req.params.name + "! It's so great to see you!")
+});
+
+app.listen(port, () => {
+    console.log("Express is listening on port;", port)
+});
