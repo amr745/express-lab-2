85015f5297c788e02196c1ec9c37ddd908129190
Ashley Richard <ashleymrichard@gmail.com>
2022-04-02 18:54:18 -0700

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -6,8 +6,11 @@ app.get("/greeting/:name", (req, res) => {
     res.send(req.params.name + "! It's so great to see you!")
 });
 
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+    res.send(`${(parseInt(req.params.total) /100) * parseInt(req.params.tipPercentage)}`)
+});
+
 app.listen(port, () => {
     console.log("Express is listening on port;", port)
 });
 
-// "Greeting express application created."
